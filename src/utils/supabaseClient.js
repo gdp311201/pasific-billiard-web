7821040d042b7a760e0fa013d591ce4a71e6f9c8
GDP <growspheredigpro@gmail.com>
2026-09-16 06:19:10 +0700

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'PASTE_URL_LU_DISINI'
-const supabaseAnonKey = 'PASTE_PUBLISHABLE_KEY_LU_DISINI'
+const supabaseUrl = 'https://supabase.com/dashboard/project/vyspjpmpsjddvibpsdsh'
+const supabaseAnonKey = 'sb_publishable_Q3MMHMSwIkx_KuVF4V_eVA_hjArMYjT'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
