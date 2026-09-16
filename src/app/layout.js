afdb04182a9383cf2333e5cfe172b565b325d323
GDP <growspheredigpro@gmail.com>
2026-09-16 07:30:34 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export const metadata = {
   title: 'Pasific Billiard x Titik Balik Cafe',
@@ -9,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body className="relative bg-black text-white min-h-screen">
+      <body className="relative bg-black text-white min-h-screen flex flex-col">
         {/* Efek Glow Background Global */}
         <div className="fixed top-0 left-1/4 w-[500px] h-[500px] bg-[#D4AF37]/10 rounded-full blur-[120px] pointer-events-none z-0"></div>
         <div className="fixed bottom-0 right-1/4 w-[500px] h-[500px] bg-yellow-700/10 rounded-full blur-[120px] pointer-events-none z-0"></div>
@@ -18,9 +19,12 @@ export default function RootLayout({ children }) {
         <Navbar />
 
         {/* Konten Halaman */}
-        <div className="relative z-10 pt-0">
+        <div className="relative z-10 flex-grow pt-0">
           {children}
         </div>
+
+        {/* Footer Global */}
+        <Footer />
       </body>
     </html>
   )
